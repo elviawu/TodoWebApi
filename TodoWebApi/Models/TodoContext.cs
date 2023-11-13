0f6e8dd76b6ead2862cc02100cb75e707fc8de9e
elviawu <elviawu@gmail.com>
2023-11-14 05:56:04 +0800

    加入專案檔案。

---
+++ TodoWebApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebApi.Models;
+
+namespace TodoWebApi.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
